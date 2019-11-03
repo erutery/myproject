762265da81408eb8489da7542d6a363f45101fb4
erutery <erutery@gmail.com>
2019-11-03 13:45:34 +0800

    variable, value, data type

--- src/main/java/com/Arson/Hello.java
+++ src/main/java/com/Arson/Hello.java
@@ -6,5 +6,11 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 28;
+        double weight = 58.9;
+        float height = 173.5f;
+        boolean enroll = true;
+        String name = "Arson";
+
     }
 }
--- src/main/kotlin/com/Arson/Hello.kt
+++ src/main/kotlin/com/Arson/Hello.kt
@@ -5,6 +5,11 @@ fun main(args: Array<String>) {
 //   Human().hello()
     val h = Human()
     h.hello()
+    var age : Long = 28
+    age = 29
+    val weight = 58.6
+    val name : String
+    name = "Arson"
 }
 
 class Human{
